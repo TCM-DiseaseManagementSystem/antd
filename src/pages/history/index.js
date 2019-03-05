b00c09aeed74bd286f6b085c8209e82da33e27fb
wangxueli <958427071@qq.com>
2019-03-05 11:51:55 +0800

    对接根据Id获取全部记录和详细记录接口

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,32 +1,8 @@
 import React, { Component } from 'react';
-import {
-    List, Avatar, Button, Skeleton, Popconfirm,
-} from 'antd';
-import {Link} from "react-router-dom";
+import { Button } from 'antd';
+import { Link } from "react-router-dom";
 import $ from "jquery";
 import './index.less';
-const Data = [
-    {
-        title: '四诊采集',
-        description: '望闻问切，体形结实，肌肉充实，皮肤润泽，表示体格强壮，正气充盛；形体瘦弱，肌肉瘦削，皮肤枯燥，表示衰弱，正气不足。',
-        time: '2018-12-20'
-    },
-    {
-        title: '认知筛查量表得分',
-        description: '100',
-        time: '2018-12-20'
-    },
-    {
-        title: 'MMSE评分量表',
-        description: '100',
-        time: '2018-12-20'
-    },
-    {
-        title: 'MoCA评分量表',
-        description: '100',
-        time: '2018-12-20'
-    }
-];
 
 export default class History extends Component{
     constructor(props) {
@@ -52,48 +28,6 @@ export default class History extends Component{
         })
     }
 
-    // selectDetaile = (data) =>{
-    //     if (data) {
-    //         if (data.QuestionnaireRecords) {
-    //             if (data.QuestionnaireRecords) {
-    //                 data.QuestionnaireRecords.map((item,index) =>{
-    //                     if (item.QuestionnaireName === '认知筛查量表得分' ) {
-    //                         {/*return(*/}
-    //                             {/*<Link to="/cognition">*/}
-    //                                 {/*<Button onClick={() => this.handleEdit}>查看</Button>*/}
-    //                             {/*</Link>*/}
-    //                         {/*)*/}
-    //                     {/*}*/}
-    //                 })
-    //             }else if (item.QuestionnaireName === 'MMSE评分量表') {
-    //                     return(
-    //                         <Link to="/mmse">
-    //                             <Button onClick={() => this.handleEdit}>查看</Button>
-    //                         </Link>
-    //                     )
-    //             } else if (data.QuestionnaireRecords.QuestionnaireName === 'MoCA评分量表') {
-    //                 return(
-    //                     <Link to="/moca">
-    //                         <Button onClick={() => this.handleEdit}>查看</Button>
-    //                     </Link>
-    //                 )
-    //             }
-    //         }else if (data.MedicalRecords) {
-    //             return(
-    //                 <Link to="/diagnosis">
-    //                     <Button onClick={() => this.handleEdit}>查看</Button>
-    //                 </Link>
-    //             )
-    //         }else if (data.GaugeRecords) {
-    //             return(
-    //                 <Link to={'/physicochemical'}>
-    //                     <Button onClick={() => this.handleEdit}>查看</Button>
-    //                 </Link>
-    //             )
-    //         }
-    //     }
-    // }
-
     getContent = (data) =>{
         if (data) {
             return (
@@ -106,7 +40,11 @@ export default class History extends Component{
                                     <span className={'detail-content-dels'}>总体正常，部分指标偏高。</span>
                                 </div>
                                 <div className={'detail-content-time'}>{item.CreatedAt}</div>
-                                {/*<div className={'detail-content-btn'}>{this.selectDetaile(data)}</div>*/}
+                                <div className={'detail-content-btn'}>
+                                    <Link to={'/physicochemical'}>
+                                        <Button onClick={() => this.handleEdit}>查看</Button>
+                                    </Link>
+                                </div>
                             </div>
                         )
                     }) : null}
@@ -115,11 +53,24 @@ export default class History extends Component{
                             <div className={'detail-content'} key={index}>
                                 <div className={'detail-content-title'}>
                                     <span className={'detail-content-titles'}>四诊信息采集</span>
-                                    <span
-                                        className={'detail-content-dels'}>望闻问切，体形结实，肌肉充实，皮肤润泽，表示体格强壮，正气充盛；形体瘦弱，肌肉瘦削，皮肤枯燥，表示衰弱，正气不足。</span>
+                                    <span className={'detail-content-dels'}>
+                                        {item.Diagnoses.map((items,indexs) =>{
+                                        return(
+                                            <div key={indexs}>{items.DiagnoseName}、</div>
+                                        )
+                                        })
+                                        }
+                                    </span>
                                 </div>
                                 <div className={'detail-content-time'}>{item.CreatedAt}</div>
-                                {/*<div className={'detail-content-btn'}>{this.selectDetaile(data)}</div>*/}
+                                <Link to={
+                                    {
+                                        pathname: '/diagnosis',
+                                        state: {Id: item.Id},
+                                    }
+                                }>
+                                    <Button onClick={() => this.handleEdit}>查看</Button>
+                                </Link>
                             </div>
                         )
                     }): null}
@@ -131,7 +82,29 @@ export default class History extends Component{
                                     <span className={'detail-content-dels'}>{item.TotalScore}</span>
                                 </div>
                                 <div className={'detail-content-time'}>{item.CreatedAt}</div>
-                                {/*<div className={'detail-content-btn'}>{this.selectDetaile(data)}</div>*/}
+                                {item.QuestionnaireId === '16c7071b-ec13-4788-bfce-123774b8e347' ?  <Link to={
+                                    {
+                                        pathname: '/cognition',
+                                        state: { Id: item.Id}
+                                    }
+                                }>
+                                    <Button onClick={() => this.handleEdit}>查看</Button>
+                                </Link> : item.QuestionnaireId === '87f5a6b4-ee86-4b2f-a984-1dc6edc77e9c' ? <Link to={
+                                    {
+                                        pathname: '/mmse',
+                                        state: { Id: item.Id}
+                                    }
+                                }>
+                                    <Button onClick={() => this.handleEdit}>查看</Button>
+                                </Link> : item.QuestionnaireId === '39c66d1e-cab6-412d-918b-5fb895e545d7' ? <Link to={
+                                    {
+                                        pathname: '/moca',
+                                        state: { Id: item.Id}
+                                    }
+                                }>
+                                    <Button onClick={() => this.handleEdit}>查看</Button>
+                                </Link> : null
+                                }
                             </div>
                         )
                     }): null}
